731b708d35b828e6c1e6c4472babb54bf8b04bd5
oyesa <mercyoyesa2018@gmail.com>
2022-05-26 16:09:26 +0300

    make migrations, create project class and update admin

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    # article_image = models.ImageField(upload_to = 'articles/', blank=True, null=True)
+   
