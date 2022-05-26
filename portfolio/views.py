a7d62668c3dd7ff8e8230aa18aec3d2222653ad6
oyesa <mercyoyesa2018@gmail.com>
2022-05-26 13:33:11 +0300

    create home template

--- portfolio/views.py
+++ portfolio/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
   return HttpResponse('My portfolio in Moringa Core')
+
+def projects(request):
+  return render(request, 'home.html')
